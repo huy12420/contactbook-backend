01e861a4197155d34cb1b5cbbe07b122500ca0b4
huy12420@ <huy12420@gmail.com>
2022-09-19 09:52:44 +0700

    configure ESLint for the project

--- src/app.js
+++ src/app.js
@@ -1,52 +1,41 @@
-const express = require('express'); 
-const cors = require('cors'); 
- 
+const express = require('express');
+const cors = require('cors');
+const ApiError = require('./api-error');
 
-const app = express(); 
-
-app.use(cors()); 
-app.use(express.json()); 
-
-app.get('/', (req,res) => { 
-    res.json({ message: 'Welcome to contact book application.'});
-});
-module.exports = app;
-//step 4
 const contactController = require('./controllers/contact.controller');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 
 app.route('/api/contacts')
-.get(contactController.findAll)
-.post(contactController.create)
-.delete(contactController.deleteAll);
+    .get(contactController.findAll)
+    .post(contactController.create)
+    .delete(contactController.deleteAll);
 
 app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
 
 app.route('/api/contacts/:id')
-.get(contactController.findOne)
-.put(contactController.update)
-.delete(contactController.delete);
-module.exports = app;
-//step 5 
+    .get(contactController.findOne)
+    .put(contactController.update)
+    .delete(contactController.delete);
 
-const ApiError = require('./api-error');
 
-app.route('/api/contacts/:id')
-.get(contactController.findOne)
-.put(contactController.update)
-.delete(contactController.delete);
-// Handle 404 response.
 app.use((req, res, next) => {
-// Handler for unknown route.
-// Call next() to pass to the error handling middleware.
-return next(new ApiError(404, 'Resource not found'));
+    return next(new ApiError(404, 'Resource not found'));
 });
-// Define error-handling middleware last, after other app.use() and routes
+
 
 app.use((err, req, res, next) => {
-// The centralized error handling middleware.
-// In any route handler, calling next(error)
-// will pass to this error handling middleware.
-return res.status(err.statusCode || 500).json({
-message: err.message || 'Internal Server Error',
+    return res.status(err.statusCode || 500).json({
+        message: err.message || 'Internal Server Error',
+    });
 });
-});
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to contact book application.' });
+}
+);
+
+module.exports = app;
